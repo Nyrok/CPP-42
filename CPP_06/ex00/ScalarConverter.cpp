d846d43c61acef01edcffc91fd1a96acc4d47769
Nyrok <nyrokgaming1@gmail.com>
2026-08-01 14:06:49 +0200

    CPP_06

--- CPP_06/ex00/ScalarConverter.cpp
+++ CPP_06/ex00/ScalarConverter.cpp
@@ -4,6 +4,7 @@
 #include <cstdlib>
 #include <cctype>
 #include <climits>
+#include <cmath>
 
 void	ScalarConverter::printChar(double value)
 {
@@ -33,12 +34,8 @@ void	ScalarConverter::printFloat(double value)
 		std::cout << "float: +inff" << std::endl;
 	else if (f == -std::numeric_limits<float>::infinity())
 		std::cout << "float: -inff" << std::endl;
-	else if (f > -1e18f && f < 1e18f && f == static_cast<float>(static_cast<long>(f)))
-	{
+	else if (std::floor(f) == f)
 		std::cout << "float: " << std::fixed << std::setprecision(1) << f << "f" << std::endl;
-		std::cout.unsetf(std::ios::fixed);
-		std::cout.precision(6);
-	}
 	else
 		std::cout << "float: " << f << "f" << std::endl;
 }
@@ -51,12 +48,8 @@ void	ScalarConverter::printDouble(double value)
 		std::cout << "double: +inf" << std::endl;
 	else if (value == -std::numeric_limits<double>::infinity())
 		std::cout << "double: -inf" << std::endl;
-	else if (value > -1e18 && value < 1e18 && value == static_cast<double>(static_cast<long>(value)))
-	{
+	else if (std::floor(value) == value)
 		std::cout << "double: " << std::fixed << std::setprecision(1) << value << std::endl;
-		std::cout.unsetf(std::ios::fixed);
-		std::cout.precision(6);
-	}
 	else
 		std::cout << "double: " << value << std::endl;
 }
--- CPP_06/ex02/main.cpp
+++ CPP_06/ex02/main.cpp
@@ -29,10 +29,8 @@ void	identify(Base *p)
 		std::cout << "A" << std::endl;
 	else if (dynamic_cast<B *>(p))
 		std::cout << "B" << std::endl;
-	else if (dynamic_cast<C *>(p))
-		std::cout << "C" << std::endl;
 	else
-		std::cout << "Unknown" << std::endl;
+		std::cout << "C" << std::endl;
 }
 
 void	identify(Base &p)
@@ -55,16 +53,7 @@ void	identify(Base &p)
 	catch (std::exception &e)
 	{
 	}
-	try
-	{
-		(void)dynamic_cast<C &>(p);
-		std::cout << "C" << std::endl;
-		return ;
-	}
-	catch (std::exception &e)
-	{
-	}
-	std::cout << "Unknown" << std::endl;
+	std::cout << "C" << std::endl;
 }
 
 int	main(void)
